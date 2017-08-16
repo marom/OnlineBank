8e910433bab6001873d1f8591db478da73da5fba
MarekLiss <marek.muszynski@liss.co.uk>
2017-08-16 19:12:45 +0100

    add a basic version of a primary account screen

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -1,11 +1,29 @@
 package com.userfront.controller;
 
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.User;
+import com.userfront.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private UserService userService;
 
+    @RequestMapping("/primaryAccount")
+    public String primaryAccount(Principal principal, Model model) {
+
+    User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+        return "primaryAccount";
+    }
 }

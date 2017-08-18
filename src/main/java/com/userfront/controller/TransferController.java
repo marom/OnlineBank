2f0303b250cdce226ace37c78347a79388e86e38
MarekLiss <marek.muszynski@liss.co.uk>
2017-08-18 18:23:19 +0100

    add GET request for transfer between accounts

---
+++ src/main/java/com/userfront/controller/TransferController.java
@@ -0,0 +1,29 @@
+package com.userfront.controller;
+
+import com.userfront.service.TransactionService;
+import com.userfront.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/transfer")
+public class TransferController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/betweenAccounts", method = RequestMethod.GET)
+    public String betweenAccounts(Model model) {
+        model.addAttribute("transferFrom", "");
+        model.addAttribute("transferTo", "");
+        model.addAttribute("amount", "");
+
+        return "transferBetweenAccounts";
+    }
+}

1c230f80a5d7e4dfa3714aa0bea806e3e6d3524e
maro.muszynski <maro.muszynski@gmail.com>
2017-08-20 22:19:10 +0200

    save modified user data

--- src/main/java/com/userfront/controller/UserController.java
+++ src/main/java/com/userfront/controller/UserController.java
@@ -5,12 +5,14 @@ import com.userfront.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.security.Principal;
 
 @Controller
+@RequestMapping(value = "/user")
 public class UserController {
 
     @Autowired
@@ -22,6 +24,25 @@ public class UserController {
         User user = userService.findByUsername(principal.getName());
         model.addAttribute("user", user);
         return "profile";
+    }
 
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String profilePost(@ModelAttribute("user") User updatedUser, Model model, Principal principal) {
+
+        User user = userService.findByUsername(principal.getName());
+        user.setUsername(updatedUser.getUsername());
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        user.setPhone(updatedUser.getPhone());
+        user.setEmail(updatedUser.getEmail());
+
+        model.addAttribute("user", user);
+        userService.save(user);
+
+        if (principal.getName().equalsIgnoreCase(updatedUser.getUsername())) {
+            return "profile";
+        } else {
+            return "redirect:/index?logout";
+        }
     }
 }
